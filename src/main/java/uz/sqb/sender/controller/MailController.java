1e74e4e3e41ecc9e6a883e0300d781d8522843b1
anvarov.javlon94 <anvarov.javlon94@gmail.com>
2021-12-27 15:07:39 +0500

    5

--- src/main/java/uz/sqb/sender/controller/MailController.java
+++ src/main/java/uz/sqb/sender/controller/MailController.java
@@ -1,32 +1,32 @@
-package uz.sqb.sender.controller;
-
-import lombok.AccessLevel;
-import lombok.experimental.FieldDefaults;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import uz.sqb.sender.service.EmailService;
-
-@RestController
-@FieldDefaults(level = AccessLevel.PRIVATE)
-public class MailController {
-
-   private final EmailService emailService;
-
-    public MailController(EmailService emailService) {
-        this.emailService = emailService;
-    }
-
-    @GetMapping("/mail")
-    public ResponseEntity sendMessage(){
-        String text = "Hello";
-        String subject = "Subject";
-        String to = "janvarov@uzpsb.uz";
-        String from = "anvarov.javlon94@gmail.com";
-        emailService.sendMessage(subject,text,to,from);
-        System.out.println("Hi");
-        return ResponseEntity.ok("Ok");
-    }
-
-
-}
+//package uz.sqb.sender.controller;
+//
+//import lombok.AccessLevel;
+//import lombok.experimental.FieldDefaults;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import uz.sqb.sender.service.EmailService;
+//
+//@RestController
+//@FieldDefaults(level = AccessLevel.PRIVATE)
+//public class MailController {
+//
+//   private final EmailService emailService;
+//
+//    public MailController(EmailService emailService) {
+//        this.emailService = emailService;
+//    }
+//
+//    @GetMapping("/mail")
+//    public ResponseEntity sendMessage(){
+//        String text = "Hello";
+//        String subject = "Subject";
+//        String to = "janvarov@uzpsb.uz";
+//        String from = "anvarov.javlon94@gmail.com";
+//        emailService.sendMessage(subject,text,to,from);
+//        System.out.println("Hi");
+//        return ResponseEntity.ok("Ok");
+//    }
+//
+//
+//}
--- src/main/java/uz/sqb/sender/service/EmailService.java
+++ src/main/java/uz/sqb/sender/service/EmailService.java
@@ -1,28 +1,28 @@
-package uz.sqb.sender.service;
-
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE)
-public class EmailService {
-
-    @Autowired
-    JavaMailSender mailSender;
-
-    public void sendMessage(String subject, String text, String sentMail, String from){
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(from);
-        message.setTo(sentMail);
-        message.setSubject(subject);
-        message.setText(text);
-        mailSender.send(message);
-    }
-
-}
+//package uz.sqb.sender.service;
+//
+//import lombok.AccessLevel;
+//import lombok.RequiredArgsConstructor;
+//import lombok.experimental.FieldDefaults;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//@FieldDefaults(level = AccessLevel.PRIVATE)
+//public class EmailService {
+//
+//    @Autowired
+//    JavaMailSender mailSender;
+//
+//    public void sendMessage(String subject, String text, String sentMail, String from){
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setFrom(from);
+//        message.setTo(sentMail);
+//        message.setSubject(subject);
+//        message.setText(text);
+//        mailSender.send(message);
+//    }
+//
+//}
