1757bc16dcdde473d4ebba14bdf26cdb1dc07bdf
anvarov.javlon94 <anvarov.javlon94@gmail.com>
2021-12-27 14:37:34 +0500

    First

---
+++ src/main/java/uz/sqb/sender/controller/MailController.java
@@ -0,0 +1,31 @@
+package uz.sqb.sender.controller;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uz.sqb.sender.service.EmailService;
+
+@RestController
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class MailController {
+
+   private final EmailService emailService;
+
+    public MailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @GetMapping("/mail")
+    public ResponseEntity sendMessage(){
+        String text = "Hello";
+        String subject = "Subject";
+        String to = "janvarov@uzpsb.uz";
+        String from = "anvarov.javlon94@gmail.com";
+        emailService.sendMessage(subject,text,to,from);
+        return ResponseEntity.ok("Ok");
+    }
+
+
+}
